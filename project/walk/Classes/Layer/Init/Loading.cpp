430acb19250e9739a60d1c4b89e8a62c726f48e3
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-05-25 23:44:49 +0800

    modified:   Classes/Layer/Init/Loading.cpp 	modified:   Classes/Layer/Init/Loading.h

--- project/walk/Classes/Layer/Init/Loading.cpp
+++ project/walk/Classes/Layer/Init/Loading.cpp
@@ -1,4 +1,4 @@
-#include "./LoadingLayer.h"
+#include "./Loading.h"
 
 namespace jiangweigithub {
 
@@ -9,17 +9,24 @@ namespace jiangweigithub {
       return false;
     }
 
-    auto loadingSprite = LoadingSprite::getLoadingSprite();
-    auto loadingTitle = LoadingSprite::getLoadingTitle();
+    // auto loadingSprite = LoadingSprite::getLoadingSprite();
+    // auto loadingTitle = LoadingSprite::getLoadingTitle();
 
-    auto spriteSize = loadingSprite->getContentSize();
-    auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
+    // auto spriteSize = loadingSprite->getContentSize();
+    // auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
 
-    loadingSprite->setPosition(cocos2d::Vec2((visibleSize.width - spriteSize.width) / 2, (visibleSize.height - spriteSize.height) / 2));
-    loadingTitle->setPosition(cocos2d::Vec2((visibleSize.width - spriteSize.width) / 2, (visibleSize.height - spriteSize.height) / 2 + 60));
+    // loadingSprite->setPosition(cocos2d::Vec2((visibleSize.width - spriteSize.width) / 2, (visibleSize.height - spriteSize.height) / 2));
+    // loadingTitle->setPosition(cocos2d::Vec2((visibleSize.width - spriteSize.width) / 2, (visibleSize.height - spriteSize.height) / 2 + 60));
 
-    this->addChild(loadingSprite);
-    this->addChild(loadingTitle);
+    // this->addChild(loadingSprite);
+    // this->addChild(loadingTitle);
+    Role::Role* role = new Role::Role();
+    std::vector<cocos2d::Rect> vec;
+    vec.push_back(cocos2d::Rect(180,0,30,50));
+    vec.push_back(cocos2d::Rect(210,0,30,50));
+    vec.push_back(cocos2d::Rect(240,0,30,50));
+    vec.push_back(cocos2d::Rect(210,0,30,50));
+    cocos2d::Animation* walkRight = role->getAnimationRight(vec);
 
     return true;
   }
